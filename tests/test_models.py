a6825551d82219a79d66fed32e70bb2b451920cc
luisdlgarza <luisdlgarza2@gmail.com>
2022-03-09 23:36:18 +0000

    Updates

--- tests/test_models.py
+++ tests/test_models.py
@@ -4,9 +4,13 @@ Test cases for YourResourceModel Model
 import logging
 import unittest
 import os
-from service.models import RecommendationModel, DataValidationError, db
+
+from flask import redirect
+from service.models import Reason, RecommendationModel, DataValidationError, db
 from service import app
 from werkzeug.exceptions import NotFound
+
+from tests.factories import RecsFactory
 
 ######################################################################
 #  <your resource name>   M O D E L   T E S T   C A S E S
@@ -44,13 +48,28 @@ class TestRecommendationModel(unittest.TestCase):
 
     def test_create_a_recommendation(self):
         """ Create a recommendation and assert that it exists """
-        rec = RecommendationModel(name="iPhone", category="dog", available=True, gender=Gender.MALE)
-        self.assertTrue(pet is not None)
-        self.assertEqual(pet.id, None)
-        self.assertEqual(pet.name, "Fido")
-        self.assertEqual(pet.category, "dog")
-        self.assertEqual(pet.available, True)
-        self.assertEqual(pet.gender, Gender.MALE)
-        pet = Pet(name="Fido", category="dog", available=False, gender=Gender.FEMALE)
-        self.assertEqual(pet.available, False)
-        self.assertEqual(pet.gender, Gender.FEMALE)
+        rec = RecommendationModel(name="iPhone", prod_A_id = 5, prod_B_name = "AirPods", prod_B_id = 10, reason = Reason.ACCESSORY )
+        self.assertTrue(rec is not None)
+        self.assertEqual(rec.id, None)
+        self.assertEqual(rec.name, "iPhone")
+        self.assertEqual(rec.prod_A_id, 5)
+        self.assertEqual(rec.prod_B_name, "AirPods")
+        self.assertEqual(rec.prod_B_id, 10)
+        self.assertEqual(rec.reason, Reason.ACCESSORY)
+        rec = RecommendationModel(name="iPhone", prod_A_id = 5, prod_B_name = "AirPods", prod_B_id = 8, reason = Reason.CROSS_SELL)
+        self.assertEqual(rec.prod_B_id, 8)
+        self.assertEqual(rec.reason, Reason.CROSS_SELL)
+
+    def test_add_a_pet(self):
+        """Create a recommendation and add it to the database"""
+        recs = RecommendationModel.all()
+        self.assertEqual(recs, [])
+        rec = RecommendationModel(name="iPhone", prod_A_id = 5, prod_B_name = "AirPods", prod_B_id = 10, reason = Reason.ACCESSORY )
+        self.assertTrue(rec is not None)
+        self.assertEqual(rec.id, None)
+        rec.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertEqual(rec.id, 1)
+        recs = RecommendationModel.all()
+        self.assertEqual(len(recs), 1)
+
