146ea7f2f27786329e8702b89ceaafa256f6154d
luisdlgarza <luisdlgarza2@gmail.com>
2022-03-09 22:49:04 +0000

    Edits

--- service/routes.py
+++ service/routes.py
@@ -12,7 +12,7 @@ from . import status  # HTTP Status Codes
 # For this example we'll use SQLAlchemy, a popular ORM that supports a
 # variety of backends including SQLite, MySQL, and PostgreSQL
 from flask_sqlalchemy import SQLAlchemy
-from service.models import YourResourceModel, DataValidationError
+from service.models import RecommendationModel, DataValidationError
 
 # Import Flask application
 from . import app
@@ -37,4 +37,4 @@ def index():
 def init_db():
     """ Initializes the SQLAlchemy app """
     global app
-    YourResourceModel.init_db(app)
+    RecommendationModel.init_db(app)
--- tests/test_models.py
+++ tests/test_models.py
@@ -5,6 +5,8 @@ import logging
 import unittest
 import os
 from service.models import RecommendationModel, DataValidationError, db
+from service import app
+from werkzeug.exceptions import NotFound
 
 ######################################################################
 #  <your resource name>   M O D E L   T E S T   C A S E S
@@ -14,26 +16,41 @@ class TestRecommendationModel(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        """ This runs once before the entire test suite """
-        pass
+        """This runs once before the entire test suite"""
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        RecommendationModel.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        pass
+        db.drop_all()  # clean up the last tests
+        db.create_all()  # make our sqlalchemy tables
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
+        db.drop_all()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_XXXX(self):
-        """ Test something """
-        self.assertTrue(True)
+    def test_create_a_recommendation(self):
+        """ Create a recommendation and assert that it exists """
+        rec = RecommendationModel(name="iPhone", category="dog", available=True, gender=Gender.MALE)
+        self.assertTrue(pet is not None)
+        self.assertEqual(pet.id, None)
+        self.assertEqual(pet.name, "Fido")
+        self.assertEqual(pet.category, "dog")
+        self.assertEqual(pet.available, True)
+        self.assertEqual(pet.gender, Gender.MALE)
+        pet = Pet(name="Fido", category="dog", available=False, gender=Gender.FEMALE)
+        self.assertEqual(pet.available, False)
+        self.assertEqual(pet.gender, Gender.FEMALE)
