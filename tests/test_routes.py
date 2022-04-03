e5aedea04f6236e0f470bf101a9db33aa7d748ca
nfa216 <nfa216@stern.nyu.edu>
2022-04-03 21:34:58 +0000

    Staged changes for routes.py and test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -62,7 +62,7 @@ class TestYourRecommendationServer(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def _create_recs(self, count):
+    def _create_recommendations(self, count):
         """Factory method to create recommendations in bulk"""
         recs = []
         for _ in range(count):
@@ -81,7 +81,7 @@ class TestYourRecommendationServer(unittest.TestCase):
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
 
-    def test_list_recs_empty(self):
+    def test_list_recommendations_empty(self):
         """Test for an empty list"""
         test_rec = RecFactory()
         logging.debug(test_rec)
@@ -91,11 +91,11 @@ class TestYourRecommendationServer(unittest.TestCase):
         # There should be no recs
         self.assertEquals(empty_response, [])
 
-    def test_list_single_recs(self):
+    def test_list_single_recommendations(self):
         """Test for a list with one rec"""
         test_rec = RecFactory()
         logging.debug(test_rec)
-        self._create_recs(1)
+        self._create_recommendations(1)
 
         resp = self.app.get(BASE_URL, content_type=CONTENT_TYPE_JSON)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
@@ -103,16 +103,16 @@ class TestYourRecommendationServer(unittest.TestCase):
         # There should be one rec
         self.assertEqual(len(rec_list), 1)
 
-    def test_list_three_recs(self):
+    def test_list_three_recommendations(self):
         """Test for a list with three recs"""
         test_rec = RecFactory()
         logging.debug(test_rec)
-        self._create_recs(3)
+        self._create_recommendations(3)
 
         resp = self.app.get(BASE_URL, content_type=CONTENT_TYPE_JSON)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
         rec_list = resp.get_json()
-        # There should be three recs
+        # There should be three recommendations
         self.assertEqual(len(rec_list), 3)
 
     def test_index(self):
@@ -122,7 +122,7 @@ class TestYourRecommendationServer(unittest.TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], "Recommendation Demo REST API Service")
         
-    def test_create_rec(self):
+    def test_create_recommendations(self):
         """Create a new Recommendation"""
         test_rec = RecFactory()
         logging.debug(test_rec)
@@ -178,7 +178,7 @@ class TestYourRecommendationServer(unittest.TestCase):
             new_rec["reason"], test_rec.reason.name, "Reasons does not match"
         )
 
-    def test_update_rec(self):
+    def test_update_recommendations(self):
         """Update an existing Recommendation"""
         # create a recommendation to update
         test_rec = RecFactory()
@@ -200,9 +200,9 @@ class TestYourRecommendationServer(unittest.TestCase):
         updated_rec = resp.get_json()
         self.assertEqual(updated_rec["name"], "Test123")
 
-    def test_delete_recommendation(self):
+    def test_delete_recommendations(self):
         """Delete a Recommendation"""
-        test_recommendation = self._create_recs(1)[0]
+        test_recommendation = self._create_recommendations(1)[0]
         resp = self.app.delete(
             "{0}/{1}".format(BASE_URL, test_recommendation.id), content_type=CONTENT_TYPE_JSON
         )
@@ -217,7 +217,7 @@ class TestYourRecommendationServer(unittest.TestCase):
     def test_get_rec(self):
         """Get a single Rec"""
         # get the id of a Rec
-        test_rec = self._create_recs(1)[0]
+        test_rec = self._create_recommendations(1)[0]
         resp = self.app.get(
             "/recommendations/{}".format(test_rec.id), content_type=CONTENT_TYPE_JSON
         )
@@ -225,14 +225,14 @@ class TestYourRecommendationServer(unittest.TestCase):
         data = resp.get_json()
         self.assertEqual(data["name"], test_rec.name)
 
-    def test_get_rec_not_found(self):
+    def test_get_recommendations_not_found(self):
         """Get a Rec thats not found"""
         resp = self.app.get("/recommendations/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_query_recommendation_list_by_original_product_id(self):
+    def test_query_recommendations_list_by_original_product_id(self):
         """Query Recommendations by Original Product ID"""
-        recs = self._create_recs(10)
+        recs = self._create_recommendations(10)
         test_original_product_id = recs[0].original_product_id
         prod_id_list = [rec for rec in recs if rec.original_product_id == test_original_product_id]
 
@@ -252,7 +252,7 @@ class TestYourRecommendationServer(unittest.TestCase):
             
     # Testing Sad Paths
 
-    # def test_recs_bad_content_type(self):
+    # def test_recommendations_bad_content_type(self):
     #     """Test for bad content type"""
     #     resp = self.app.get(BASE_URL, content_type='image/jpeg')
     #     self.assertEqual(resp.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
@@ -261,7 +261,7 @@ class TestYourRecommendationServer(unittest.TestCase):
     #     self.assertEqual(error_response["message"], "415 Unsupported Media Type: Content-Type must be application/json")
     #     self.assertEqual(error_response["status"], 415)
         
-    def test_create_rec_bad_request(self):
+    def test_create_recommendations_bad_request(self):
         """Test for bad request"""
         resp = self.app.post(
             BASE_URL, json='123', content_type=CONTENT_TYPE_JSON
@@ -271,7 +271,7 @@ class TestYourRecommendationServer(unittest.TestCase):
         self.assertEqual(error_response["message"], "Invalid recommendation: body of request contained bad or no data string indices must be integers")
         self.assertEqual(error_response["status"], 400)
 
-    def test_recs_not_found(self):
+    def test_recommendations_not_found(self):
         """Test for unknown path"""
         resp = self.app.get("testing123", content_type=CONTENT_TYPE_JSON)
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
@@ -280,7 +280,7 @@ class TestYourRecommendationServer(unittest.TestCase):
         self.assertEqual(error_response["message"], "404 Not Found: The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.")
         self.assertEqual(error_response["status"], 404)
 
-    def test_recs_method_unsupported(self):
+    def test_recommendations_method_unsupported(self):
         """Test for unsupported method"""
         resp = self.app.put(BASE_URL, content_type=CONTENT_TYPE_JSON)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
